96ca08a4ef9d703c9fdf8ced4117dc336227918d
unknown <zhlocal@hbmlos-brooke.(none)>
2012-08-10 11:36:50 -0700

    Now we can create a view and see the settings page that follows.

--- src/main/java/jenkins/plugins/zmon/ZMonView.java
+++ src/main/java/jenkins/plugins/zmon/ZMonView.java
@@ -1,14 +1,22 @@
 package jenkins.plugins.zmon;
+import hudson.Extension;
 import hudson.Plugin;
-import hudson.model.ListView;
-import hudson.model.Job;
-import hudson.model.Hudson;
-import hudson.model.ViewDescriptor;
+import hudson.model.*;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
+import javax.servlet.ServletException;
 
 
 public class ZMonView extends ListView{
-  /**
+
+    private Integer numColumns = 1;
+    @DataBoundConstructor
+    public ZMonView(String name, Integer numColumns) {
+        super(name);
+        this.numColumns = numColumns != null ? numColumns : 1;
+    }
+    /**
    * C'tor<meta  />
    * @param name the name of the view
    * @param numColumns the number of columns to use on the layout (work in progress)
@@ -21,7 +29,7 @@ public class ZMonView extends ListView{
    * Notify Hudson we're implementing a new View
    * @author jrenaut
    */
-//  @Extension
+  @Extension
   public static final class ZMonViewDescriptor extends ViewDescriptor {
 
     @Override
@@ -30,4 +38,7 @@ public class ZMonView extends ListView{
     }
 
   }
+
+
+
 }
