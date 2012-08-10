8126424722ab665c824e269833245a4b67808a11
Steve Moyer <moyer.steve@gmail.com>
2012-08-10 10:50:57 -0700

    added ViewDescriptor

--- src/main/java/jenkins/plugins/zmon/ZMonView.java
+++ src/main/java/jenkins/plugins/zmon/ZMonView.java
@@ -3,8 +3,31 @@ import hudson.Plugin;
 import hudson.model.ListView;
 import hudson.model.Job;
 import hudson.model.Hudson;
+import hudson.model.ViewDescriptor;
+
+
+
 public class ZMonView extends ListView{
-  public ZMonView (String name) {
+  /**
+   * C'tor<meta  />
+   * @param name the name of the view
+   * @param numColumns the number of columns to use on the layout (work in progress)
+   */
+  public ZMonView(String name) {
     super(name);
+  }
+
+  /**
+   * Notify Hudson we're implementing a new View
+   * @author jrenaut
+   */
+//  @Extension
+  public static final class ZMonViewDescriptor extends ViewDescriptor {
+
+    @Override
+    public String getDisplayName() {
+      return "Zynx Monitor";
+    }
+
   }
 }
