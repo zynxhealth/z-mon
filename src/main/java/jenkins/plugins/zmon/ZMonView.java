de0c47301f62e97e5301dbc946ddde6b7a45ac68
unknown <zhlocal@hbmlos-brooke.(none)>
2012-08-10 16:50:48 -0700

    A lot of work by Alex T., Steve M., and Aslan B. to get a demo of the Z-Mon Build Monitor almost up to par.

--- src/main/java/jenkins/plugins/zmon/ZMonView.java
+++ src/main/java/jenkins/plugins/zmon/ZMonView.java
@@ -1,6 +1,5 @@
 package jenkins.plugins.zmon;
 import hudson.Extension;
-import hudson.Plugin;
 import hudson.model.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -11,10 +10,13 @@ import java.io.IOException;
 
 public class ZMonView extends ListView{
 
+    private String teamName;
+
     @DataBoundConstructor
     public ZMonView(String name) {
         super(name);
     }
+
     /**
    * Notify Hudson we're implementing a new View
    * @author jrenaut
@@ -35,5 +37,81 @@ public class ZMonView extends ListView{
         super.submit(req);
   }
 
+    public String getTeamName() {
+        if (this.teamName == null) {
+            this.teamName = "Team Z-Mon";
+        }
+        return  this.teamName;
+    }
 
+
+    public String getBuildTime() {return getJobName("zMon_Build"); }
+    public String getDeployTime() { return getJobName("zMon_Deploy"); }
+    public String getTestsTime() { return getJobName("zMon_Test"); }
+    public String getMatureJobName() {return getJobName("zMon_Mature"); }
+    public String getRegressionJobName() { return getJobName("zMon_Regression"); }
+
+    public String getBuildTimeUnit() {return getTimeUnit(); }
+    public String getDeployTimeUnit() { return getTimeUnit(); }
+    public String getTestsTimeUnit() { return getTimeUnit(); }
+    public String getMatureTimeUnit() {return getTimeUnit(); }
+    public String getRegressionTimeUnit() { return getTimeUnit(); }
+
+    public String getBuildSinceLastRun() { return getSinceLastRun("zMon_Build"); }
+    public String getDeploySinceLastRun() { return getSinceLastRun("zMon_Deploy"); }
+    public String getTestsSinceLastRun() { return getSinceLastRun("zMon_Test"); }
+    public String getMaturedSinceLastRun() { return getSinceLastRun("zMon_Mature"); }
+    public String getRegressionSinceLastRun() { return getSinceLastRun("zMon_Regression"); }
+
+    public String getBuildStatus() { return getStatus("zMon_Build"); }
+    public String getDeployStatus() { return getStatus("zMon_Deploy"); }
+    public String getTestsStatus() { return getStatus("zMon_Test"); }
+    public String getMatureStatus() { return getStatus("zMon_Mature"); }
+    public String getRegressionStatus() { return getStatus("zMon_Regression"); }
+
+    public String getBuildStatus2() { return getStatus2("zMon_Build"); }
+    public String getDeployStatus2() { return getStatus2("zMon_Deploy"); }
+    public String getTestsStatus2() { return getStatus2("zMon_Test"); }
+    public String getMatureStatus2() { return getStatus2("zMon_Mature"); }
+    public String getRegressionStatus2() { return getStatus2("zMon_Regression"); }
+
+
+    public String getSinceLastRun(String jobName) {
+        Project tli = (Project)(Hudson.getInstance().getItem(jobName));
+        return String.valueOf((System.currentTimeMillis() - tli.getLastBuild().getTimeInMillis()) / 60000) + " mins";
+    }
+
+    private String getJobName(String jobName) {
+        Project tli = (Project)(Hudson.getInstance().getItem(jobName));
+        return String.valueOf(tli.getLastBuild().getDuration()/60000);
+    }
+
+    public String getTimeUnit() {
+       return "mins";
+  }
+
+  public String getStatus(String jobName) {
+      Project tli = (Project)(Hudson.getInstance().getItem(jobName));
+      if (tli.isBuilding()) {
+          return "running";
+      }
+      else {
+          if (tli.getLastBuild().getBuildStatusSummary().message.toString().equalsIgnoreCase("stable")) {
+              return "passed";
+          }
+          else {
+              return "failed";
+          }
+      }
+  }
+
+  public String getStatus2(String jobName) {
+      Project tli = (Project)(Hudson.getInstance().getItem(jobName));
+      if (tli.isBuilding()) {
+          return "running-time";
+      }
+      else {
+          return "";
+      }
+  }
 }
